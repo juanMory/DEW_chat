a4ac285bb48c89fca70219715e9922ecd7d10b4b
Juan Mory <spacejamm@gmail.com>
2012-01-30 22:36:26 -0500

    eliminado

--- xmas_gifts/app/models/gift.rb
+++
@@ -1,4 +0,0 @@
-class Gift < ActiveRecord::Base
-  belongs_to :person
-  validates_presence_of :name, :person_id
-end
--- xmas_gifts/db/migrate/20120127013112_add_price_to_gifts.rb
+++
@@ -1,5 +0,0 @@
-class AddPriceToGifts < ActiveRecord::Migration
-  def change
-    add_column :gifts, :price, :integer, :default =>0, :null=>false
-  end
-end
